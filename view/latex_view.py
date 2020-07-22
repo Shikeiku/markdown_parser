ffe04b91f794c393c96564732c2ef85c938a1704
Mike Vink <mike@MacBook-Air.domain_not_set.invalid>
2020-07-22 09:14:28 +0200

    updated definitions

--- view/latex_view.py
+++ view/latex_view.py
@@ -11,94 +11,99 @@ class LaTeX_viewer():
         self.scratch = Path(
             '/Users/mike/.data/nvim/scratch_files/LaTeX/scratch.tex')
         self.fencenames = ['LaTeX', 'python', 'anki-cloze']
+        self.regex_dict = {
+            'chapter': r'^#\s(?P<name>.*)',
+            'section': r'^##\s(?P<name>.*)',
+            'subsection': r'^###\s(?P<name>.*)',
+            'subsubsection': r'^####\s(?P<name>.*)',
+            'paragraph': r'^#####\s(?P<name>.*)',
+            'subparagraph': r'^######\s(?P<name>.*)',
+            'code': r'(?P<name>^```.*)'
+        }
+        self.function_dict = {
+            'chapter': self.latexSections,
+            'section': self.latexSections,
+            'subsection': self.latexSections,
+            'subsubsection': self.latexSections,
+            'paragraph': self.latexSections,
+            'subparagraph': self.latexSections,
+            'code': self.codeBlock
+        }
+        self.code_dict = {
+            'LaTeX': {
+                'begin': '\n',
+                'end': '\n'
+            },
+            'python': {
+                'begin': '\\begin{listings}',
+                'end': '\\end{listings}'
+            }
+        }
 
-    def section(self, line):
+    def latexRegexDictionary(self, line):
         """
         Is called in the main function to transform lines with headers to
         latex chapters or sections
         """
-        CHAPTER_REGEX = r'^#\s(?P<name>.*)'
-        SECTION_REGEX = r'^##\s(?P<name>.*)'
-        SUBSECTION_REGEX = r'^###\s(?P<name>.*)'
-        SUBSUBSECTION_REGEX = r'^####\s(?P<name>.*)'
-        PARAGRAPH_REGEX = r'^#####\s(?P<name>.*)'
-        SUBPARAGRAPH_REGEX = r'^######\s(?P<name>.*)'
+        for name, regex in self.regex_dict.items():
+            target = re.search(regex, line)
+            if target:
+                named_target = [name, target]
+                rv = named_target
+                break
+            else:
+                rv = line
+        return rv
 
-        chapter = re.search(CHAPTER_REGEX, line)
-        section = re.search(SECTION_REGEX, line)
-        subsection = re.search(SUBSECTION_REGEX, line)
-        subsubsection = re.search(SUBSUBSECTION_REGEX, line)
-        paragraph = re.search(PARAGRAPH_REGEX, line)
-        subparagraph = re.search(SUBPARAGRAPH_REGEX, line)
+    def latexFunctionDictionary(self, named_target):
+        if isinstance(named_target, list):
+            func = self.function_dict.get(named_target[0], self.lineNotDefined)
+            return func, named_target
+        else:
+            return self.lineNotDefined, named_target
 
-        if chapter:
-            line = r'\chapter{' + chapter.group('name') + '}'
-        elif section:
-            line = r'\section{' + section.group('name') + '}'
-        elif subsection:
-            line = r'\subsection{' + subsection.group('name') + '}'
-        elif subsubsection:
-            line = r'\subsubsection{' + subsubsection.group('name') + '}'
-        elif paragraph:
-            line = r'\paragraph{' + paragraph.group('name') + '}'
-        elif subparagraph:
-            line = r'\subparagraph{' + subparagraph.group('name') + '}'
-
+    def lineNotDefined(self, line):
         return line
 
-    def code_block(self, line, current_block=None):
+    def latexSections(self, named_target):
+        line = '\\' + named_target[0] + '{' + named_target[1].group(
+            'name') + '}'
+        return line
+        # print('hello')
+
+    def codeBlock(self, line):
         """
         Transforms the lines containing fences of code blocks to latex listings
         things or deletes the fences if the fences are latex
         """
-        CODE_BLOCK_REGEX = r'(?P<prefix>^```(?P<name>' + '|'.join(
-            self.fencenames) + ')?)'
+        global current_code
+        line = line[1].group('name')
+        code_regex = r'(?P<prefix>^```(?P<name>' + '|'.join(
+            self.fencenames) + '))'
+        code_match = re.search(code_regex, line)
+        if code_match:
+            current_code = code_match.group('name')
+            line = self.code_dict[code_match.group('name')]['begin']
+        elif current_code in self.code_dict:
+            line = self.code_dict[current_code]['end']
+            current_code = ''
+        return line
 
-        # if :
-        # else:
-        #     return None
-
-    def input_handler(self, relative_link):
-        """
-        Opens the markdown file in the link, and writes the contents of the
-        file as part of the whole document.
-        """
-
-    def file_link(self, link):
-        """
-        Puts the path in the markdown file in a \href{...}
-        """
-
-    def citation(self, unknown):
-        """
-        Need to figure out how to write down a citation in markdown files
-
-        probably could write \citealp
-        """
-
-    def image(self, link):
-        """
-        Link to an image should probably become a template where the absolute
-        path to the image is substituted in
-        """
-
-    def write_contents(self, lines):
-        """
-        Takes the list that was build up with all other functions and writes 
-        it to the scratch file
-        """
-
-    def view_latex(self):
+    def view_latex(self, files='current'):
         """
         Scans over the current file and uses the methods in the class to
         convert the markdown to a latex file
         """
-        lines = self.nvim.current.buffer[:]
+        if files == 'current':
+            lines = self.nvim.current.buffer[:]
+
         for i in range(len(lines)):
-            if self.section(lines[i]):
-                lines[i] = self.section(lines[i])
-            if self.code_block(lines[i]):
-                lines[i], code_block = self.code_block(lines[i])
+            target = self.latexRegexDictionary(lines[i])
+            function, named_target = self.latexFunctionDictionary(target)
+            lines[i] = function(named_target)
+
+            # if self.code_block(lines[i]):
+            #     lines[i], code_block = self.code_block(lines[i])
 
         with open(self.scratch, 'w') as s:
             for line in lines:
