308db87fb58e93db08e75d6be9fd9d92a2bee8cb
Mike Vink <mike@MacBook-Air.home>
2020-08-23 16:30:31 +0200

    Added binder.py

---
+++ binder.py
@@ -0,0 +1,16 @@
+class poopi_shelajeiofj(wioejoij):
+    """
+    Docstring for poopi_shelajeiofj
+    """
+    def __init__(self, a, b, c, d, e):
+        """@todo: to be defined.
+
+        :a: @todo
+        :b: @todo
+        :c: @todo
+        :d: @todo
+        :e: @todo
+
+        """
+
+    
