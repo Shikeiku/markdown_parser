6977b7447d7a4211263c31f3871844faf5b8cc68
Mike Vink <mikevink@Mikes-Air.domain_not_set.invalid>
2020-08-26 16:50:36 +0200

    Added functions exploring the console api

--- rich_functions.py
+++ rich_functions.py
@@ -1,4 +1,7 @@
-from vnnv.console import console
+from rich.console import Console, OverflowMethod
+
+console = Console(width=100, record=True)
+
 
 def console_attributes():
     """
@@ -10,5 +13,56 @@ def console_attributes():
     color_sys = console.color_system
     console.print(locals())
 
+
+def logging_and_printing():
+    """
+    @todo Docstring for myFunction
+    """
+    hello = "hello, world"
+    # console.print(hello)
+    console.log(hello, log_locals=True)
+
+
+def justify_or_align(style="bold white on blue", **kwargs):
+    """
+    You can also set the console attributes of course
+    """
+    console.print("Rich", style=style, justify="left")
+    console.print("Rich", style=style, justify="center")
+    console.print("Rich", style=style, justify="right")
+    console.log(style, log_locals=True)
+
+
+def overflow(console=Console(width=14),
+             string="supercalifragilisticexpialidocious"):
+    """
+    @todo Docstring for overflow
+    """
+    overflow_methods: List[OverflowMethod] = ["fold", "crop", "ellipsis"]
+    for overflow in overflow_methods:
+        console.rule(overflow)
+        console.print(string, overflow=overflow, style="bold blue")
+        console.print()
+
+
+def rich_input(string="What is [b]your[/b] [bold red]name[/]? :smiley: "):
+    """
+    @todo Docstring for rich_input
+    """
+    console.input(string)
+
+
 if __name__ == "__main__":
+    console.print("\nprinting console_attributes")
     console_attributes()
+    console.print("\nprinting logging and printing")
+    logging_and_printing()
+    console.print("\nprinting justify_or_align")
+    justify_or_align()
+    console.print("\noverflow:")
+    overflow()
+    console.print("\nrich_input")
+    rich_input()
+    # text = console.export_text(clear=True)
+
+
