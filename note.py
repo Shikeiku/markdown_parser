ac759f718ba93cb7b3bc98429e6bb0a2ae828513
Vink <mike1994vink@gmail.com>
2020-08-31 16:39:38 +0200

    Feat: list notes by tag sorted by date

--- note.py
+++ note.py
@@ -21,7 +21,18 @@ class Note:
         """
         @todo: Docstring for _init_preamble
         """
+        # Here there should be some code ensuring that the note could be a
+        # string of the title of the note, a string of the absolute path, and a
+        # pathlib object.
+        #
+        #
         # print(self.n)
+        # try:
+        #     if not Path(self.n).exists():
+        #         self.n = b.path / self.n
+        # except:
+        #     raise('Note must be title string or posixpath')
+
         with open(self.n, 'r') as n:
             first = n.readline()
             # preamble = first
@@ -39,6 +50,25 @@ class Note:
                     preamble_string += line
         if preamble:
             preamble = {}
+            # '20200818-13h20m40s_Moller_Struth_chapter_2'
+
+            match = re.search(r'\d{8}\-\d{2}h\d{2}m\d{2}s\_(.*)',
+                              str(self.n.name))
+            if match:
+                preamble['title'] = match.group(1)
+
+            match = re.search(
+                r'(\d{4})(\d{2})(\d{2})\-(\d{2})h(\d{2})m(\d{2})s\_.*',
+                str(self.n.name))
+            if match:
+                preamble['date'] = {
+                    'year': match.group(1),
+                    'month': match.group(2),
+                    'day': match.group(3),
+                    'hour': match.group(4),
+                    'minute': match.group(5),
+                    'second': match.group(6)
+                }
             # console.print(preamble_string)
 
             # console.print(preamble_string, style='info')
@@ -57,13 +87,16 @@ class Note:
                 # console.print(Panel.fit(preamble['tags'], style='succes'))
                 # console.print(preamble_string, style='info')
                 # console.print(preamble, style='info')
-                preamble['tags'] = [tag.strip().lower() for tag in match.group(1).split(',')]
+                preamble['tags'] = [
+                    tag.strip().lower() for tag in match.group(1).split(',')
+                ]
 
             match = re.search(r'links = \{([\s\S]*?)\}', preamble_string)
             if match:
                 # preamble['links'] = match.group(1)
-                preamble['links'] = [link for link in
-                        match.group(1).strip().split('\n')]
+                preamble['links'] = [
+                    link for link in match.group(1).strip().split('\n')
+                ]
             # preamble = preamble_string
 
         # console.print(preamble)
