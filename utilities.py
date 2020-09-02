ab61b13f116ff8d9ae092287adfa2f1b4b6964ff
Mike Vink <mikevink@Mikes-Air.domain_not_set.invalid>
2020-09-02 14:17:18 +0200

    Progress: Preparation for anki flashcards

--- utilities.py
+++ utilities.py
@@ -1,12 +1,14 @@
-from subprocess import call
 import tempfile
 import os
+import subprocess
+from subprocess import call
+import re
 
-from vnnv.config import console
+from vnnv.config import cfg, console
+
 
 class cd:
     """Context manager for changing the current working directory"""
-
     def __init__(self, newPath):
         self.newPath = os.path.expanduser(newPath)
         self.savedPath = None
@@ -61,3 +63,50 @@ def choose(items, text="Choose from list:"):
             return reply
         except IndexError:
             continue
+
+
+def pdflatex(lines, latex_build_dir=cfg['latex']['build_dir']) -> None:
+    """
+    @todo: Docstring for pdflatex
+
+    /args/ @todo
+
+    """
+    with tempfile.NamedTemporaryFile(mode='w+',
+                                     prefix='latex_note_',
+                                     suffix='.tex',
+                                     delete=False) as tf:
+        tf.write(lines)
+        tf.flush()
+        with cd(os.path.expandvars(latex_build_dir)):
+            with open('latex_wrapper.tex', 'r+') as wrapper:
+                lines = wrapper.read()
+                # console.print(lines)
+                lines = re.sub(r'(begin[\s\S]*?\\input{).*?(})',
+                               r'\1' + tf.name + r'\2', lines)
+                # console.print(lines)
+                wrapper.seek(0)
+                wrapper.write(lines)
+                wrapper.flush()
+            subprocess.call(
+                ['pdflatex', '--interaction=batchmode', 'latex_wrapper.tex'])
+            subprocess.call(['open', '-a', 'skim', 'latex_wrapper.pdf'])
+
+
+def apy_add_from_file(lines, tags=None) -> None:
+    """
+    @todo: Docstring for apy_add_from_file
+
+    /args/ @todo
+
+    """
+    with tempfile.NamedTemporaryFile(mode='w+',
+                                     prefix='vnnv-anki_',
+                                     suffix='.md',
+                                     delete=False) as tf:
+        tf.write(lines)
+        tf.flush()
+        if tags is not None:
+            call(['apy', 'apy_add_from_file', '-t', tags, tf.name])
+        else:
+            call(['apy', 'apy_add_from_file', tf.name])
